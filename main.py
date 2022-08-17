8481f02c3203584ed645e809db4cc2f95c9c5740
lucaspy93 <>
2022-08-17 17:34:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# ><><><>< What I've learned in Day1: ><><><>< #
+
+# print()
+# print('text between quotes or double quotes.')
+print("Hello World\n")
+print("Hello\nWorld!\n")
+print("Hello " + "World" + "!\n")
+print("'Hello World in simples quotes'")
+print('"Hello World in double quotes"')
+print("Use '\\n' to break the line or the input bar.\n")
+
+# input()
+# input('text that asks the user for an information.')
+input("What is your name? ")
+input("\nHow old are you?\n")
+
+# len()
+# len(variable or text that this function will return its length.)
+print(len(input("Write something and I'll return its length: ")))
